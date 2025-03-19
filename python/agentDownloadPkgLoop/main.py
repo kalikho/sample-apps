b58cbb775b9a69a00d4c549b5fec2a0b3bda6e92
Roshan Singh <roshansingh3000@gmail.com>
2025-03-19 16:14:56 +0530

    resolved conflict with upstream main

--- python/agentDownloadPkgLoop/main.py
+++ python/agentDownloadPkgLoop/main.py
@@ -6,8 +6,11 @@ import grpc
 import calendar
 import time
 import os
+<<<<<<< HEAD
 import string
 import random
+=======
+>>>>>>> 6c99b40 (added files)
 
 
 addr = 'localhost:4040'  # update the server and port accordingly, addr corresponds to an agent
@@ -42,8 +45,12 @@ def registerIdentity(identity_name):
         print("Identity Name : ",param3)
         print("Timestamp : ", param2)
         print(" ")
+<<<<<<< HEAD
         print("Switch to the App, approve identity, and set artifacts")
     return param1
+=======
+        print("Switch to the App and approve identity")
+>>>>>>> 6c99b40 (added files)
 
 
 def downloadpkg(identity,storagePath):
@@ -54,6 +61,7 @@ def downloadpkg(identity,storagePath):
         if not os.path.exists(param2):
             os.makedirs(param2)
 
+<<<<<<< HEAD
         while(1):
             counter = counter + 1
             ireq = edgeagent_pb2.DownloadFilesReq(identity=param1,path=param2)
@@ -63,6 +71,14 @@ def downloadpkg(identity,storagePath):
                 print("Download attempt - ",counter)
             except Exception as e:
                 print("error: ",e.details)
+=======
+        print("==== downloading ====")
+        while(1):
+            counter = counter + 1
+            print("Download attempt - ",counter)
+            ireq = edgeagent_pb2.DownloadFilesReq(identity=param1,path=param2)
+            req = stub.DownloadFiles(ireq)
+>>>>>>> 6c99b40 (added files)
             time.sleep(3)
     except grpc.RpcError as e:
         print(f'failed setting: {e.details}')
@@ -71,9 +87,30 @@ def downloadpkg(identity,storagePath):
         print("Package downloaded at location: ",param2)
 
 if __name__ == '__main__':
+<<<<<<< HEAD
         N = 7
         identityname = ''.join(random.choices(string.ascii_uppercase +
                              string.digits, k=N))
         identity = registerIdentity(identityname)
         storagePath = "downloads/"+identity
         downloadpkg(identity,storagePath)
+=======
+    while(1):
+        print(" ")
+        print("1. Generate Identity")
+        print("2. Download Package")
+        print("3. Exit")
+        print(" ")
+        choice = input('Select your operation ')
+        print(" ")
+        
+        if choice == "1":
+            name = input("Identity Name ")
+            registerIdentity(name)
+        if choice == "2":
+            identity = input("Identity DID : ")
+            storagePath = input("Storage Path : ")
+            downloadpkg(identity,storagePath)
+        if choice == "3":
+            exit()
+>>>>>>> 6c99b40 (added files)
